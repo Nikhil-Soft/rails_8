bf148a14a23fd35b2e70e7d663df5e40a3fd66c0
Nikhil Sahu <nikhilsahu6005@gmail.com>
2025-01-11 19:36:44 +0530

    small fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,19 +5,17 @@ class Product < ApplicationRecord
   validates :name, presence: true
 
   has_one_attached :featured_image
-  # validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
+  validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
 
-  # def self.import_from_api
-  #   service = ProductApiService.new
-  #   debugger
-  #   products = service.fetch_products
+  def self.import_from_api
+    service = ProductApiService.new
+    products = service.fetch_products
 
-  #   products.each do |product_data|
-  #     Product.find_or_create_by(external_id: product_data['id']) do |product|
-  #       product.name = product_data['title']
-  #       # product.description = product_data['description']
-  #       # product.price = product_data['price']
-  #     end
-  #   end
-  # end
+    products.each do |product_data|
+      Product.find_or_create_by(id: product_data['id']) do |product|  
+        product.name = product_data['title']
+        product.description = product_data['description']
+      end
+    end
+  end
 end
